3134c98692662b778ed76c9dfd0e1ec94472a808
ghostenginesupport <ghostengine.support@gmail.com>
2026-04-24 17:40:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,13 +16,28 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
     console.log("User Connected:", socket.id);
 
+    // Room join
     socket.on("join_room", (room) => {
         socket.join(room);
         console.log(socket.id + " joined room: " + room);
     });
 
-    socket.on("update_skin", (data) => {
-        console.log("Skin Update:", data);
+    // PRO Skin Sync (video / gif / png / jpg)
+    socket.on("sync_pro_skin", (data) => {
+        console.log("Pro Skin Sync:", data);
+
+        /*
+        Expected data:
+        {
+            room: "ghost-dimension-test",
+            playerId: "socket-id",
+            skinId: "imgurSkinId",
+            videoUrl: "https://i.imgur.com/xxxxx.mp4",
+            x: 1234,
+            y: 5678,
+            size: 240
+        }
+        */
 
         socket.to(data.room).emit("render_new_skin", data);
     });

e5e87a3ab3fe9d8d5be1b7bf73705f73ecd56e87
ghostenginesupport <ghostengine.support@gmail.com>
2026-04-24 17:26:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http, {
-  cors: { origin: "*" }
+
+const http = require("http").createServer(app);
+
+const io = require("socket.io")(http, {
+    cors: {
+        origin: "*"
+    }
 });
 
-app.get('/', (req, res) => {
-  res.send('Ghost Network Server is Running!');
+app.get("/", (req, res) => {
+    res.send("Ghost Network Server Running");
 });
 
-io.on('connection', (socket) => {
-  console.log('User Connected:', socket.id);
+io.on("connection", (socket) => {
+    console.log("User Connected:", socket.id);
 
-  socket.on('join_room', (room) => {
-    socket.join(room);
-    console.log(`Socket ${socket.id} joined room: ${room}`);
-  });
+    socket.on("join_room", (room) => {
+        socket.join(room);
+        console.log(socket.id + " joined room: " + room);
+    });
 
-  socket.on('update_skin', (data) => {
-    // data must have { room, skinUrl, playerId }
-    socket.to(data.room).emit('render_new_skin', data);
-  });
+    socket.on("update_skin", (data) => {
+        console.log("Skin Update:", data);
 
-  socket.on('disconnect', () => {
-    console.log('User Disconnected');
-  });
+        socket.to(data.room).emit("render_new_skin", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Disconnected:", socket.id);
+    });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log('Server running on port ' + PORT));
+
+http.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
